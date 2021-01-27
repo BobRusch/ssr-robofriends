221701f46b775af5d43afaa820167fb32dbd7583
Bob Rusch <rvrusch@gmail>
2021-01-27 16:14:56 -0500

    Added meta-tags

--- containers/Home.js
+++ containers/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Head from 'next/head';
 import { connect } from 'react-redux';
 import { setSearchField, requestRobots } from '../redux/actions';
 
@@ -37,6 +38,33 @@ class HomePage extends Component {
     });
     return (
       <div className='tc'>
+        <Head>
+          <meta charset='utf-8' />
+          <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+          <meta
+            name='viewport'
+            content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'
+          />
+          <meta name='description' content='Description' />
+          <meta name='keywords' content='Keywords' />
+          <title>Next.js PWA Example</title>
+
+          <link rel='manifest' href='/manifest.json' />
+          <link
+            href='/favicon-16x16.png'
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+          />
+          <link
+            href='/favicon-32x32.png'
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+          />
+          <link rel='apple-touch-icon' href='/apple-icon.png'></link>
+          <meta name='theme-color' content='#317EFB' />
+        </Head>
         <h1 className='f1'>RoboFriends</h1>
         <SearchBox searchChange={onSearchChange} />
         <Scroll>
